1a8b2f88429594edce48100cfd93895503dc7321
steven sanchez <1steven.nathan.sanchez@gmail.com>
2024-03-14 19:34:04 -0600

    Se definio el modelo Usuario

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    ROLES = (
+        ('admin', 'Admin'),
+        ('films', 'Films'),
+        ('people', 'People'),
+        ('locations', 'Locations'),
+        ('species', 'Species'),
+        ('vehicles', 'Vehicles'),
+    )
+    nombre = models.CharField(max_length=100)
+    rol = models.CharField(max_length=20, choices=ROLES)
+    
+    def __str__(self):
+        return self.nombre
