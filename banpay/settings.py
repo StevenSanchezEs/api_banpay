b9298c64134b1b68482a32fc92ef50f66a987635
steven sanchez <1steven.nathan.sanchez@gmail.com>
2024-03-15 23:40:20 -0600

    Update - Se configuro variable de entorno para activar o desactivar el modo DEBUG

--- banpay/settings.py
+++ banpay/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-34hdbp&tde8_+zd2k)q$+u=6r0z0v1ms4pqpm&7uv@nn=_85g='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True'),
 
 ALLOWED_HOSTS = []
 
